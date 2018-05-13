9747f011e5376e7e503272a28f1ecdfe418a2bd4
greylable <greylable@gmail.com>
2018-05-14 00:27:58 +0800

    Add Checkbox UI to Voidenvelopes

--- app/models/voidenvelope.rb
+++ app/models/voidenvelope.rb
@@ -6,14 +6,7 @@ class Voidenvelope < ApplicationRecord
   def self.import(file, user)
     CSV.foreach(file.path, headers:true) do |row|
       puts row[0]
-      # puts Voidenvelope.errors.full_messages
       v = Voidenvelope.create(envelope_id: row[0], name: row[1], void_reason: row[2], user: user)
-      # v.save!
-
-      # t.string "name"
-      # t.string "envelope_id"
-      # t.string "void_reason"
-      # t.integer "user_id"
     end
   end
 end
