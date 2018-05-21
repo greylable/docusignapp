33a037b74371cdf33053477ba1fca9ec3d45eaa2
greylable <greylable@gmail.com>
2018-05-21 21:36:53 +0800

    WIP UI Fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,8 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     collection do
       post :import
       post :destroy_multiple
+      post :hihi
     end
   end
 
