dc89936ecc3018c97cd77461ceea1899414b2146
greylable <greylable@gmail.com>
2018-06-17 17:34:02 +0800

    Temp remove Refresh and add Google API for Masterlist

--- app/controllers/masterlists_controller.rb
+++ app/controllers/masterlists_controller.rb
@@ -28,7 +28,8 @@ class MasterlistsController < ApplicationController
 
   def refresh
     # head :ok
-    Masterlist.refresh_masterlist
+    # Masterlist.refresh_masterlist
+    Masterlist.g_auth
     respond_to do |format|
       format.html { redirect_to root_url }
       format.json { head :no_content }
